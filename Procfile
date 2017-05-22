e0fdac11a93fd1844a8816fe2af25a6e9a9615b2
Stephen Boyer <sboyer307@gmail.com>
2017-05-21 18:02:15 -0600

    Trying to get db to work on remote

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/sbhome2-1.0.jar
--- src/main/java/com/stephenboyer/sbhome2/Main.java
+++ src/main/java/com/stephenboyer/sbhome2/Main.java
@@ -36,6 +36,23 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
 
+/*
+ * Copyright 2002-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 @Controller
 @SpringBootApplication
 public class Main {
@@ -47,7 +64,6 @@ public class Main {
   private DataSource dataSource;
 
   public static void main(String[] args) throws Exception {
-
     SpringApplication.run(Main.class, args);
   }
 
@@ -64,7 +80,7 @@ public class Main {
       stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
       ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-      ArrayList<String> output = new ArrayList<>();
+      ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
         output.add("Read from DB: " + rs.getTimestamp("tick"));
       }
