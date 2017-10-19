15a96477f2e06a9a3e2abe8e4a5e54eae1910811
Steve Boyer <sboyer307@gmail.com>
2017-10-18 20:32:36 -0600

    Fixed problems caused by http to https redirect changes

--- src/main/java/com/stephenboyer/sbhome2/SecurityConfig.java
+++ src/main/java/com/stephenboyer/sbhome2/SecurityConfig.java
@@ -4,18 +4,16 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 import javax.inject.Inject;
 
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Inject
-    private SecurityProperties securityProperties;
+    @Inject private SecurityProperties securityProperties;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        if (securityProperties.isRequireSsl()) http.requiresChannel().anyRequest().requiresSecure();
+//        if (securityProperties.isRequireSsl()) http.requiresChannel().anyRequest().requiresSecure();
     }
 }
