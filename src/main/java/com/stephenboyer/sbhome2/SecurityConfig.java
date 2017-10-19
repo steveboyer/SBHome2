349a4739fbe9f493954c2ebcd96bdc5dea0b2e37
Steve Boyer <sboyer307@gmail.com>
2017-10-18 20:09:16 -0600

    SecurityConfig class added for https redirect

---
+++ src/main/java/com/stephenboyer/sbhome2/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.stephenboyer.sbhome2;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.inject.Inject;
+
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Inject
+    private SecurityProperties securityProperties;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        if (securityProperties.isRequireSsl()) http.requiresChannel().anyRequest().requiresSecure();
+    }
+}
